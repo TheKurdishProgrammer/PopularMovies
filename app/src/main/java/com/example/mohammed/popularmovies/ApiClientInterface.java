67ac61ea879595a6a9897e5a914df4b9a18451c2
TheKurdishProgrammer <mahamadhusen93@gmail.com>
2018-07-06 01:45:24 +0300

    Popular Movie Initial Commit for the First Stage

--- app/src/main/java/com/example/mohammed/popularmovies/ApiClientInterface.java
+++ app/src/main/java/com/example/mohammed/popularmovies/ApiClientInterface.java
@@ -1,7 +1,5 @@
 package com.example.mohammed.popularmovies;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -11,9 +9,6 @@ public interface ApiClientInterface {
     String apiKey = "api_key=22c697a4410340a4449fd31b6b5d451b";
     String mostPopular = "popularity.desc";
     String topRated = "vote.average.desc";
-
-    @POST("movie?" + apiKey)
-    Call<List<Movie>> getTopRated();
 
 
     @POST("movie?" + apiKey)
