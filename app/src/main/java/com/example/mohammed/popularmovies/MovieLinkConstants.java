2736d50343684fbf0b46a052c2384ba4b1ccef32
TheKurdishProgrammer <mahamadhusen93@gmail.com>
2018-07-06 16:13:17 +0300

    added some modification according to udacity Review

--- app/src/main/java/com/example/mohammed/popularmovies/MovieLinkConstants.java
+++ app/src/main/java/com/example/mohammed/popularmovies/MovieLinkConstants.java
@@ -5,5 +5,4 @@ public interface MovieLinkConstants {
     String movieImgURL = "http://image.tmdb.org/t/p/w185/";
     String baseMovieURL = "http://api.themoviedb.org/3/movie/";
     String apiKey = "api_key=YOUR_API_KEY";
-
 }
