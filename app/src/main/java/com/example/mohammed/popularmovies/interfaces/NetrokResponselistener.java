8166d77b7fe4471af3b1fdc1f1de92987340857c
TheKurdishProgrammer <mahamadhusen93@gmail.com>
2018-08-01 13:39:34 +0300

    The Second version(Stage 2) of the PopularMovies App

--- app/src/main/java/com/example/mohammed/popularmovies/interfaces/NetrokResponselistener.java
+++ app/src/main/java/com/example/mohammed/popularmovies/interfaces/NetrokResponselistener.java
@@ -3,6 +3,5 @@ package com.example.mohammed.popularmovies.interfaces;
 public interface NetrokResponselistener<Response> {
 
     void onResponseReceived(Response response);
-
     void onError();
 }
